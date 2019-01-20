7b6625cd31d3d57cc98e00ee5fd1426f2da6648d
Peter <petecruckshank@gmail.com>
2019-01-19 21:10:46 -0500

    init fetchData to get the dataset for D3.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hello World")
+
+// Fetch Dataset from freeCodeCamp API 
+const fetchData = () =>  {
+   fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json")
+  .then(response => response.json())
+  .then(data => {
+    console.dir(data);
+    d3.select('#container').append('svg')
+      .attr('width', "900px")
+      .attr('height', '600px')
+      .style('background', "#dff0d8");
+
+
+
+
+  })
+  .catch(err => console.log(err));
+}
+
+fetchData();
+
