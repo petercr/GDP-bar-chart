50d863bfaecc89fee54e4527362c269f687a8b5c
Peter <petecruckshank@gmail.com>
2019-01-21 16:16:52 -0500

    started playing around with d3 inside fetch()

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import * as d3 from "d3";
 
-
 // Fetch Dataset from freeCodeCamp API 
 const fetchData = () =>  {
    fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json")
   .then(response => response.json())
   .then(data => {
     console.dir(data);
-    d3.select('container').append('svg')
+    d3.select('container')
+    .enter()
+    .data(data)  
+    .append('svg')
       .attr('width', "900px")
       .attr('height', '600px')
       .style('background', "#dff0d8");
@@ -21,3 +23,4 @@ const fetchData = () =>  {
 
 fetchData();
 
+const TestData = [1,5,10,15,20,25];
