28cc39379304917d6e6a4fb51d9779e50e14261b
Peter <petecruckshank@gmail.com>
2019-01-25 19:58:43 -0500

    used a test set of data to see if it appended to the page

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 import * as d3 from "d3";
+const TestData = [1,5,10,15,20,25];
+let Dataset;
+
 
 // Fetch Dataset from freeCodeCamp API 
 const fetchData = () =>  {
    fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json")
   .then(response => response.json())
   .then(data => {
+    Dataset = data;
     console.dir(data);
-    d3.select('container')
-    .enter()
-    .data(data)  
-    .append('svg')
-      .attr('width', "900px")
-      .attr('height', '600px')
-      .style('background', "#dff0d8");
+    console.log(Dataset.data[0][1]);
+    d3.select("#container")
+      .data(TestData)
+      .enter()
+      .append('div')
+      .text(d => d)
+    
+    // d3.select('container')
+    // .data(data)  
+    // .enter()
+    // .append('div')
+    //   .attr('width', "900px")
+    //   .attr('height', '600px')
+    //   .style('background-color', "#dff0d8") 
+    //   .text(d => d.data[0][1] );
 
 
 
@@ -22,5 +34,3 @@ const fetchData = () =>  {
 }
 
 fetchData();
-
-const TestData = [1,5,10,15,20,25];
